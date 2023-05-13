b59cf246b6c02ef0a34c909f0009b829a37aab57
Luis E. Sanchez Jr <113560203+lsan31296@users.noreply.github.com>
2023-05-13 19:01:55 -0400

    Initialized create-react-app, deleted unnecessary files and implemented table using react-data-table-component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { fetchData } from './api';
+import DataTable from 'react-data-table-component';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [tableData, setTableData] = useState(null);
+
+  const columnHeaders = [
+    { name: 'Issuer', selector: row => row.issuer },
+    { name: 'Loan Amount', selector: row => row.loan_amount },
+    { name: 'State Code', selector: row => row.state_code },
+    { name: 'Market Value', selector: row => row.market_value },
+    { name: 'Pool Number', selector: row => row.pool_number },
+    { name: 'Fund Ticker', selector: row => row.fund_ticker },
+    { name: 'Security Group', selector: row => row.security_group },
+    { name: 'Security Type', selector: row => row.security_type },
+    { name: 'Security Sector', selector: row => row.security_sector },
+    { name: 'Weight', selector: row => row.weight },
+  ]
+
+  useEffect(() => {
+    const loadData = async() => {
+      fetchData().then(setTableData);
+    };
+
+    loadData();
+  }, []);
+
+  console.log("Table Date", tableData);
+
+  if (!tableData) {
+    return <h1>Loading...</h1>
+  } else {
+    return (
+      <div >
+        <DataTable
+          columns={columnHeaders}
+          data={tableData}
+        />
+      </div>
+    );
+  }
+
+
 }
 
 export default App;
---
+++ src/api.js
@@ -0,0 +1,6 @@
+/* This file represents the API we would be using to fetch our data from */
+import data from './data.json';
+
+export async function fetchData() {
+    return data;
+}
